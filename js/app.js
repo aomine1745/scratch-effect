9f5af94acd25dad7e1c31080fde7d338f4539964
aomine <xomarxpl07@outlook.es>
2019-11-05 20:37:57 -0500

    v 1.0

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$('#scratch').wScratchPad({
+  size        : 100,          // The size of the brush/scratch.
+  bg          : 'img.jpg',  // Background (image path or hex color).
+  fg          : '#000'
+});
